639c488cd2ea81a1f7495d356fbc7c6efecc7bee
Jonathan Garza <83787061+jonathanrgarza@users.noreply.github.com>
2021-06-03 01:56:06 -0500

    Add TimeUtility class and interface

---
+++ NovaTimer.Common/Utilities/ITimeUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace NovaTimer.Common.Utilities
+{
+    /// <summary>
+    ///     Interface for a time utility.
+    /// </summary>
+    public interface ITimeUtility
+    {
+        /// <summary>
+        ///     Tries to parse a given string into an appropriate
+        ///     <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="text">The text to parse.</param>
+        /// <param name="timeSpan">The parsed <see cref="TimeSpan"/>.</param>
+        /// <param name="ex">The exception when parsing failed.</param>
+        /// <returns>
+        ///     True if the parsing was successful, false otherwise.
+        /// </returns>
+        public bool TryParseTimeSpan(string text, out TimeSpan timeSpan,
+            out Exception ex);
+    }
+}
---
+++ NovaTimer.Common/Utilities/TimeUtility.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+
+namespace NovaTimer.Common.Utilities
+{
+    /// <summary>
+    ///     The utility class for time operations.
+    /// </summary>
+    public class TimeUtility : ITimeUtility
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="TimeUtility"/>.
+        /// </summary>
+        public TimeUtility()
+        {
+        }
+
+        /// <inheritdoc/>
+        public bool TryParseTimeSpan(string text, out TimeSpan timeSpan,
+            out Exception ex)
+        {
+            timeSpan = TimeSpan.MinValue;
+            ex = null;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                ex = new ArgumentException(
+                    $"{nameof(text)} is null or empty");
+                return false;
+            }
+
+            //Format: HH:MM:SS
+
+            if (text.Contains(':') == false)
+            {
+                //Try to parse as a single number then (seconds)
+                return TryParseSeconds(text, out timeSpan, out ex);
+            }
+
+            //Supported time formats
+            var formats = new string[]
+            {
+                "mm:ss",
+                "hh:mm:ss"
+            };
+
+            var culture = CultureInfo.CurrentCulture;
+            var result = TimeSpan.TryParseExact(text, formats, culture,
+                out timeSpan);
+
+            if (result == false)
+            {
+                ex = new FormatException("Invalid text format");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryParseSeconds(string text, out TimeSpan timeSpan,
+            out Exception ex)
+        {
+            ex = null;
+
+            var culture = CultureInfo.CurrentCulture;
+            var result = TimeSpan.TryParseExact(text, "ss", culture,
+                out timeSpan);
+
+            if (result == false)
+            {
+                ex = new FormatException("Invalid text format");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
